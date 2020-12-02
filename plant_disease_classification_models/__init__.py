01d8004a3b2c6936126564ea069d639d85020270
Abdullah Selek <abdullahselek@gmail.com>
2020-12-02 20:00:02 +0000

    Create init

---
+++ plant_disease_classification_models/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""ML models for plant disease classification."""
+
+from __future__ import absolute_import
+
+__author__ = "Abdullah Selek"
+__email__ = "abdullahselek@gmail.com"
+__copyright__ = "Copyright (c) 2020 Abdullah Selek"
+__license__ = "MIT License"
+__version__ = "0.1.0"
+__url__ = "https://github.com/abdullahselek/plant-disease-classification-models"
+__download_url__ = "https://github.com/abdullahselek/plant-disease-classification-models"
+__description__ = "ML models for plant disease classification."
