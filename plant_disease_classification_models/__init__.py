d5daf21a9d5a8d8d8da568fdef344da77a792f5f
Abdullah Selek <abdullahselek@gmail.com>
2020-12-08 20:50:23 +0000

    Update download url

--- plant_disease_classification_models/__init__.py
+++ plant_disease_classification_models/__init__.py
@@ -10,7 +10,7 @@ __copyright__ = "Copyright (c) 2020 Abdullah Selek"
 __license__ = "MIT License"
 __version__ = "0.1.0"
 __url__ = "https://github.com/abdullahselek/plant-disease-classification-models"
-__download_url__ = "https://github.com/abdullahselek/plant-disease-classification-models"
+__download_url__ = "https://pypi.org/project/plant-disease-classification-models"
 __description__ = "ML models for plant disease classification."
 
 from pkg_resources import resource_filename
